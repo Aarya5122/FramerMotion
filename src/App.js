250e64945a4f3baccdd710e82c6bede8ab45fc79
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2022-01-31 22:47:57 +0530

    Tutorial-1
    Structured all the routes and UI of the application. Basic version of application is ready without framer motion.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Routes } from "react-router-dom"
+
+import Base from "./pages/Base" //FIXME:TODO:
+import Home from "./pages/Home"
+import Order from "./pages/Order"
+import PageNotFound from "./pages/PageNotFound"
+import Toppings from "./pages/Toppings"
+import Header from "./components/Header"
+const App =() => {
+
+  const [pizza, setPizza] = useState({base:"", toppings:[]})
+
+  const addBase = (base) => {
+    setPizza({...pizza, base})
+  }
+
+  const addTopping = (topping) => {
+    let newTopping = []
+    if(!pizza.toppings.includes(topping)){
+      newTopping = [...pizza.toppings, topping]
+    } else {
+      newTopping = pizza.toppings.filter(item => item !== topping)
+    }
+    setPizza({...pizza, toppings: newTopping})
+  }
+
+  return(
+    <>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/base" element={<Base addBase={addBase} pizza={pizza}/>}/>
+        <Route path="/toppings" element={<Toppings addTopping={addTopping} pizza={pizza}/>}/>
+        <Route path="/order" element={<Order pizza={pizza}/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
